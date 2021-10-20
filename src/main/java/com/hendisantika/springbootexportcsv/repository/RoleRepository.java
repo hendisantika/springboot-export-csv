d09b599e27ea523c90cb9fa390112304896013ee
Hendi Santika <hendisantika@gmail.com>
2021-10-20 07:22:14 +0700

    #51 - Add RoleRepository

---
+++ src/main/java/com/hendisantika/springbootexportcsv/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootexportcsv.repository;
+
+import com.hendisantika.springbootexportcsv.domain.Role;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-export-csv
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/10/21
+ * Time: 07.21
+ */
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+}
