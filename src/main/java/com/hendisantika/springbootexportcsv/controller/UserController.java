bd3f51b5fea1a3bf127406a835e6d49a04249015
Hendi Santika <hendisantika@gmail.com>
2021-10-20 07:34:20 +0700

    #56 - Add list user API in UserController

--- src/main/java/com/hendisantika/springbootexportcsv/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootexportcsv/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.hendisantika.springbootexportcsv.controller;
 
 import com.hendisantika.springbootexportcsv.domain.User;
+import com.hendisantika.springbootexportcsv.domain.User2;
 import com.hendisantika.springbootexportcsv.service.UserService;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,13 +30,14 @@ import javax.servlet.http.HttpServletResponse;
  * Date: 2019-08-08
  * Time: 07:05
  */
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class UserController {
     private UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
 
     @GetMapping("/export-users")
     public void exportCSV(HttpServletResponse response) throws Exception {
@@ -47,6 +58,32 @@ public class UserController {
 
         //write all users to csv file
         writer.write(userService.listUsers());
+    }
+
+    @GetMapping("/users/export")
+    public void exportToCSV(HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".csv";
+        response.setHeader(headerKey, headerValue);
+
+        List<User2> listUsers = userService.listAll();
+
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] csvHeader = {"User ID", "E-mail", "Full Name", "Roles", "Enabled"};
+        String[] nameMapping = {"id", "email", "fullName", "roles", "enabled"};
+
+        csvWriter.writeHeader(csvHeader);
+
+        for (User2 user : listUsers) {
+            csvWriter.write(user, nameMapping);
+        }
+
+        csvWriter.close();
 
     }
+
 }
